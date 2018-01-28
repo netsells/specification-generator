56dac109731f121dab7c1822af2b0c2a75c92c67
James Judd <james.judd@netsells.co.uk>
2018-01-28 19:27:32 +0000

    add test for migration from swagger spec

--- tests/GenerateMigrationTest.php
+++ tests/GenerateMigrationTest.php
@@ -4,11 +4,12 @@ namespace Juddling\Parserator\Tests;
 
 use Juddling\Parserator\Generators\MigrationGenerator;
 use Juddling\Parserator\Parsers\OpenApiParser;
+use Juddling\Parserator\Parsers\SwaggerParser;
 use PHPUnit\Framework\TestCase;
 
 class GenerateMigrationTest extends TestCase
 {
-    public function testCreateDomainMigration()
+    public function testOpenApiMigration()
     {
         $parser = new OpenApiParser(__DIR__ . '/openapi/spec.yaml');
         $models = $parser->models();
@@ -19,5 +20,21 @@ class GenerateMigrationTest extends TestCase
 
         $this->assertContains('$table->string(\'name\')', $migration);
         $this->assertContains('Schema::create(\'domains\'', $migration);
+    }
+
+    public function testSwaggerMigration()
+    {
+        $parser = new SwaggerParser(__DIR__ . '/swagger/watchlotto.yaml');
+        $models = $parser->models();
+
+        foreach ($models as $model) {
+            if ($model->getName() === 'Watch') {
+                $generator = new MigrationGenerator($model);
+                $migration = $generator->generate();
+
+                $this->assertContains('$table->string(\'name\')', $migration);
+                $this->assertContains('Schema::create(\'watches\'', $migration);
+            }
+        }
     }
 }
