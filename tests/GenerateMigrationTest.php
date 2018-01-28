f6d178496506d3ac60bcb1ad1e390a4aadb82ddb
James Judd <james.judd@netsells.co.uk>
2018-01-28 19:33:30 +0000

    add assertions for User migration

--- tests/GenerateMigrationTest.php
+++ tests/GenerateMigrationTest.php
@@ -28,12 +28,29 @@ class GenerateMigrationTest extends TestCase
         $models = $parser->models();
 
         foreach ($models as $model) {
-            if ($model->getName() === 'Watch') {
-                $generator = new MigrationGenerator($model);
-                $migration = $generator->generate();
+            switch ($model->getName()) {
+                case 'Watch':
+                    $generator = new MigrationGenerator($model);
+                    $migration = $generator->generate();
 
-                $this->assertContains('$table->string(\'name\')', $migration);
-                $this->assertContains('Schema::create(\'watches\'', $migration);
+                    $this->assertContains('$table->string(\'name\')', $migration);
+                    $this->assertContains('Schema::create(\'watches\'', $migration);
+
+                    break;
+                case 'User':
+                    $generator = new MigrationGenerator($model);
+                    $migration = $generator->generate();
+
+                    $this->assertContains('Schema::create(\'users\'', $migration);
+
+                    $this->assertContains('$table->string(\'first_name\')', $migration);
+                    $this->assertContains('$table->string(\'last_name\')', $migration);
+                    $this->assertContains('$table->string(\'email\')', $migration);
+                    $this->assertContains('$table->string(\'phone\')', $migration);
+                    $this->assertContains('$table->boolean(\'marketing\')', $migration);
+                    $this->assertContains('$table->string(\'profile_image\')', $migration);
+
+                    break;
             }
         }
     }
