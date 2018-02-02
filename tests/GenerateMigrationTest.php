889735c835f940f3aae9a327ffad6764a6305880
James Judd <james.judd@netsells.co.uk>
2018-02-02 11:38:53 +0000

    remove open api

--- tests/GenerateMigrationTest.php
+++ tests/GenerateMigrationTest.php
@@ -9,19 +9,6 @@ use PHPUnit\Framework\TestCase;
 
 class GenerateMigrationTest extends TestCase
 {
-    public function testOpenApiMigration()
-    {
-        $parser = new OpenApiParser(__DIR__ . '/openapi/spec.yaml');
-        $models = $parser->models();
-        $domainModel = $models[0];
-
-        $generator = new MigrationGenerator($domainModel);
-        $migration = $generator->generate();
-
-        $this->assertContains('$table->string(\'name\')', $migration);
-        $this->assertContains('Schema::create(\'domains\'', $migration);
-    }
-
     public function testSwaggerMigration()
     {
         $parser = new SwaggerParser(__DIR__ . '/swagger/watchlotto.yaml');
--- tests/ParseSpecTest.php
+++ tests/ParseSpecTest.php
@@ -13,21 +13,6 @@ use PHPUnit\Framework\TestCase;
 
 class ParseSpecTest extends TestCase
 {
-    public function testOpenApiSpec()
-    {
-        $parser = new OpenApiParser(__DIR__ . '/openapi/spec.yaml');
-        $models = $parser->models();
-        $this->assertCount(2, $models);
-
-        $firstModel = $models[0];
-        $this->assertSame('Domain', $firstModel->getName());
-        $this->assertCount(2, $firstModel->getFields());
-
-        $secondModel = $models[1];
-        $this->assertSame('SupplierDomain', $secondModel->getName());
-        $this->assertCount(2, $secondModel->getFields());
-    }
-
     public function testInvalidDataType()
     {
         $this->expectException(\UnexpectedValueException::class);
